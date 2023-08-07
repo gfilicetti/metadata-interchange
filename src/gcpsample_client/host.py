0c99e754f3b783a9b465d3cf6788faabf73f2f7c
Gino Filicetti <ginof@google.com>
2023-08-07 17:46:44 -0400

    first test of host.py creating a manager plugin

--- src/gcpsample_client/host.py
+++ src/gcpsample_client/host.py
@@ -7,10 +7,10 @@ class ExamplesHost(HostInterface):
     A minimal host implementation.
     """
     def identifier(self):
-        return "org.openassetio.examples"
+        return "google.openassetio.examples"
 
     def displayName(self):
-        return "OpenAssetIO Examples"
+        return "Google OpenAssetIO Examples"
 
 # For simplicity, use a filtered console logger, this logs to
 # stderr based on the value of OPENASSETIO_LOGGING_SEVERITY.
@@ -29,3 +29,38 @@ host_interface = ExamplesHost()
 # We can now create an OpenAssetIO ManagerFactory. The ManagerFactory
 # allows us to query the available managers, and pick one to talk to.
 managerFactory = ManagerFactory(host_interface, factory_impl, logger)
+
+availableManagers = managerFactory.availableManagers()
+
+print(availableManagers)
+exit(0)
+
+# {
+#     'org.openassetio.example.manager':
+#         ManagerFactory.ManagerDetail(
+#             identifier='org.openassetio.example.manager',
+#             displayName='Example Asset Manager',
+#             info={})
+# }
+
+# Once we know which manager we wish to use, we can ask the factory
+# to create one for us.
+manager = managerFactory.createManager('org.openassetio.example.manager')
+
+# We now have an instance of the requested manager, but it is not
+# quite ready for use yet. The manager returned by the
+# ManagerFactory needs to be initialized before it can be used to
+# query or publish assets. Setup is split into two stages to allow
+# adjustments to its settings to be made prior to use if required.
+
+# A manager's current (or in this case default) settings can be
+# queried if needed:
+settings = manager.settings()
+# ...and updated with new values as desired.
+settings["server"] = "my.server.com"
+
+# Finally, we can initialize the manager with the desired settings,
+# preparing it for use. Note that this may include non-trivial
+# amounts of work. Settings updates are sparse, so if you don't have
+# any custom settings, you can pass an empty dictionary here.
+manager.initialize(settings)
