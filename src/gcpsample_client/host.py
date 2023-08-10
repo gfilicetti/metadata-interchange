a13fcf0113bd22c37796ba1a72f11798a962bbbd
Gino Filicetti <ginof@google.com>
2023-08-10 14:04:09 -0400

    Added code to test resolving a reference

--- src/gcpsample_client/host.py
+++ src/gcpsample_client/host.py
@@ -1,6 +1,8 @@
 from openassetio.log import ConsoleLogger, SeverityFilter
 from openassetio.hostApi import HostInterface, Manager, ManagerFactory
 from openassetio.pluginSystem import PythonPluginSystemManagerImplementationFactory
+from openassetio import Context
+from openassetio_mediacreation.traits.content import LocatableContentTrait
 
 class ExamplesHost(HostInterface):
     """
@@ -54,3 +56,29 @@ settings = manager.settings()
 # amounts of work. Settings updates are sparse, so if you don't have
 # any custom settings, you can pass an empty dictionary here.
 manager.initialize(settings)
+
+# Note: this will raise an exception if given a string that is not
+# recognized by this manager as a valid entity reference (ValueError
+# in Python, std::domain_error in C++). Consider
+# createEntityReferenceIfValid, if unsure of the string.
+entity_reference = manager.createEntityReference("location")
+
+# All calls to the manager must have a Context, these should always
+# be created by the target manager. The Context expresses the host's
+# intent, and ensure that any manager state is properly managed
+# between API calls.
+context = manager.createContext()
+
+# We describe what we want to do with the asset
+context.access = context.access.kRead
+
+# We describe the lifetime of the returned reference
+# as persistent retention may require a more stable value.
+context.retention = context.retention.kTransient
+
+# We can now resolve a token we may have if it is a reference. In
+# this example, we'll attempt to resolve the LocatableContent trait
+# for the entity.
+resolved_asset = manager.resolve(
+        entity_reference, {LocatableContentTrait.kId}, context)
+url = LocatableContentTrait(resolved_asset).getLocation()  # May be None
